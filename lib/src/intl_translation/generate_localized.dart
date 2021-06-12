d7eff902aad8741a78f5a5e224d45e709ecaf5a8
James Leahy <defuncart@gmail.com>
2021-06-12 11:40:08 +0200

    Remove always_use_package_imports from messages_en etc which isn't needed

--- lib/src/intl_translation/generate_localized.dart
+++ lib/src/intl_translation/generate_localized.dart
@@ -202,7 +202,6 @@ class MessageGeneration {
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
 // ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
-// ignore_for_file: always_use_package_imports
 
 import 'package:$intlImportPath/intl.dart';
 import 'package:$intlImportPath/message_lookup_by_library.dart';
