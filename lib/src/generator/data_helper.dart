59dc6bf39ec44791f4fdc7afcf2405c7aafc8d03
kecson <kecson@qq.com>
2021-07-28 15:08:22 +0800

    process key meta info.

--- lib/src/generator/data_helper.dart
+++ lib/src/generator/data_helper.dart
@@ -1,11 +1,28 @@
+var testArb = <String, dynamic>{
+  'login': 'Login',
+  '@login': {'description': 'Login page title'},
+  'settings': {
+    'title': 'Settings',
+    '@title': {'description': 'Settings page title'},
+    'faq': {
+      'title': 'Help',
+      '@title': {'description': 'Help page title'}
+    }
+  }
+};
+
 Map<String, dynamic> processData(Map<String, dynamic> data) {
   var result = <String, dynamic>{};
   data.forEach((key, value) {
-    if (value is Map) {
-      //add support value is a Map
-      //transform key
-      var entries = value.entries.map((e) => MapEntry('${key}_${e.key}', e.value));
-      var innerMap = Map.fromEntries(entries);
+    if (!key.startsWith('@') && value is Map) {
+      var innerMap = Map.fromEntries(value.entries.map((e) {
+        if (e.key.toString().startsWith('@')) {
+          //process key meta info.
+          var newKey = '@${key}_${e.key.toString().replaceFirst('@', '')}';
+          return MapEntry(newKey, e.value);
+        }
+        return MapEntry('${key}_${e.key}', e.value);
+      }));
       result.addAll(processData(innerMap));
     } else {
       result[key] = value;
